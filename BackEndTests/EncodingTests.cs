4a0eb347a1a3be7d9c8d9fbf3fd496a2a0fc6535
Eugene Popov <empopov@edu.hse.ru>
2018-03-06 22:20:35 +0500

    created encoding tests
    tests are failed

---
+++ BackEndTests/EncodingTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BackEnd;
+using BackEnd.Tree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackEndTests
+{
+    [TestClass]
+    public class EncodingTests
+    {
+        [TestMethod]
+        public void EncodingTest1()
+        {
+            FileStream fileStream = new FileStream("FrequencyTableTest.in", FileMode.Open, FileAccess.Read);
+            byte[] bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, (int)fileStream.Length);
+            FrequencyTable frequencyTable = new FrequencyTable(bytes);
+
+            HuffmanTree huffmanTree = new HuffmanTree(frequencyTable);
+            EncodingTable encodingTable = new EncodingTable(huffmanTree);
+            Console.WriteLine(encodingTable.ToString());
+
+        }
+    }
+}
