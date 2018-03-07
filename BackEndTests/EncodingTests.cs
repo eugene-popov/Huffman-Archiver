2284a77bc9794901c2cb4bdc8c629518f981e3ba
Eugene Popov <empopov@edu.hse.ru>
2018-03-07 21:00:03 +0500

    tests project recreated, some tests implemented

--- BackEndTests/EncodingTests.cs
+++ BackEndTests/EncodingTests.cs
@@ -24,5 +24,78 @@ namespace BackEndTests
             Console.WriteLine(encodingTable.ToString());
 
         }
+
+        [TestMethod]
+        public void EncodingTest2()
+        {
+            GenerateByteStream("EncodingTest2", 15, 15, false);
+            FileStream fileStream = new FileStream("EncodingTest2", FileMode.Open, FileAccess.Read);
+            byte[] bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, (int)fileStream.Length);
+            FrequencyTable frequencyTable = new FrequencyTable(bytes);
+
+            HuffmanTree huffmanTree = new HuffmanTree(frequencyTable);
+            EncodingTable encodingTable = new EncodingTable(huffmanTree);
+            Console.WriteLine(encodingTable.ToString());
+        }
+
+        [TestMethod]
+        public void EncodingTest3()
+        {
+            GenerateByteStream("EncodingTest3", 15, 16, false);
+            FileStream fileStream = new FileStream("EncodingTest3", FileMode.Open, FileAccess.Read);
+            byte[] bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, (int)fileStream.Length);
+            FrequencyTable frequencyTable = new FrequencyTable(bytes);
+
+            HuffmanTree huffmanTree = new HuffmanTree(frequencyTable);
+            EncodingTable encodingTable = new EncodingTable(huffmanTree);
+            Console.WriteLine(encodingTable.ToString());
+        }
+        [TestMethod]
+        public void EncodingTest4()
+        {
+            GenerateByteStream("EncodingTest4", 0, 16, true);
+            FileStream fileStream = new FileStream("EncodingTest4", FileMode.Open, FileAccess.Read);
+            byte[] bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, (int)fileStream.Length);
+            FrequencyTable frequencyTable = new FrequencyTable(bytes);
+            Console.WriteLine(frequencyTable.ToString());
+
+            HuffmanTree huffmanTree = new HuffmanTree(frequencyTable);
+            EncodingTable encodingTable = new EncodingTable(huffmanTree);
+            Console.WriteLine(encodingTable.ToString());
+        }
+
+        private void GenerateByteStream(string filename, int min, int max, bool random)
+        {
+            FileStream fileStream = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            BitWriter bitWriter = new BitWriter(fileStream);
+            Random rand = new Random();
+            for (int @byte = min; @byte <= max; @byte++)
+            {
+                if (random)
+                {
+                    for (int i = 0; i < rand.Next(5000); i++)
+                    {
+                        for (int _numBitsRemaining = 7; _numBitsRemaining >= 0; _numBitsRemaining--)
+                        {
+                            bitWriter.Write((@byte >> _numBitsRemaining) & 1);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int _numBitsRemaining = 7; _numBitsRemaining >= 0; _numBitsRemaining--)
+                    {
+                        bitWriter.Write((@byte >> _numBitsRemaining) & 1);
+                    }
+                }
+                
+               
+            }
+            bitWriter.Dispose();
+            fileStream.Dispose();
+        }
     }
 }
---
+++ BackEndTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BackEndTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BackEndTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ac531df5-798d-4e6f-a253-e577c925723c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
